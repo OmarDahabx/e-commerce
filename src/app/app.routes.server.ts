e4d14dd32846204ce1b764b5f3ea5233b1cdc5f7
omar dahab <omar.dahabxx@gmail.com>
2025-08-26 22:03:49 +0300

    edit on app.routes.server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server
   }
 ];
